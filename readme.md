08ecb28261626bb1e647f80d4e7da63b53b6fd6a
vini.wessner@gmail.com <vini.wessner@gmail.com>
2024-11-02 10:39:10 -0300

    created validations

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 Este projeto utiliza Puppeteer, uma biblioteca para controlar o Chrome ou Chromium através do Node.js, para automatizar o processo de login no WhatsApp Web. O script preenche o número de telefone, clica no botão "Avançar" e captura o código que deve ser inserido no celular.
 
-## Tarefas
 
-- [ ] Criar front onde o usuário inputa o numero sem o 55
-- [ ] Criar tela que será exibida o codigo que recebemos na variável 'codigo'
+<div style="display: flex; flex-wrap: wrap; gap: 10px;">
+    <img src="/assets/ia.mp4" alt="Menu" width="900">
+</div>
--- server.js
+++ server.js
@@ -1,45 +1,81 @@
-const puppeteer = require('puppeteer');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const path = require("path");
 
-(async () => {
+const app = express();
+app.use(express.json());
 
+// Configura a pasta estática para servir o index.html
+app.use(express.static(path.join(__dirname)));
 
-  const number = '980258482';
+app.post("/get-code", async (req, res) => {
+  const { number } = req.body;
 
+  if (!number) {
+    return res.status(400).json({ error: "Digite seu numero" });
+  }
 
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  console.log('Navegando para o WhatsApp Web...');
-  await page.goto('https://web.whatsapp.com'); 
+    await page.setViewport({ width: 1280, height: 800 });
 
-  console.log('Aguardando botão de login...');
-  await new Promise(resolve => setTimeout(resolve, 5000)); //necessário aguardar para que o whatsapp obtenha o pais de origem e preencha sozinho
-  await page.waitForSelector('span[role="button"][tabindex="0"].x1n68mz9');
-  await page.click('span[role="button"][tabindex="0"].x1n68mz9');
+    await page.goto("https://web.whatsapp.com");
+    await new Promise(resolve => setTimeout(resolve, 5000)); 
 
+    const phoneButtonSelector = 'span[role="button"][tabindex="0"].x1n68mz9';
+    const phoneInputSelector = 'input[aria-label="Insira seu número de telefone."]';
+    const continueButtonSelector = '#app > div > div.landing-wrapper > div.landing-window > div.landing-main > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.xdt5ytf.x1qjc9v5 > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.xeuugli.x2lwn1j.xozqiw3.xamitd3.x12fk4p8.x1hq5gj4 > button';
+    
+    while (true) {
+      try {
+        await page.waitForSelector(phoneButtonSelector, { timeout: 5000 });
+        await page.click(phoneButtonSelector);
 
-  console.log('Aguardando campo de telefone...');
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  await page.waitForSelector('input[aria-label="Insira seu número de telefone."]');
+        await new Promise(resolve => setTimeout(resolve, 5000)); 
+        await page.waitForSelector(phoneInputSelector, { timeout: 5000 });
+        await page.type(phoneInputSelector, number);
 
+        await new Promise(resolve => setTimeout(resolve, 5000)); 
+        await page.waitForSelector(continueButtonSelector, { timeout: 5000 });
+        await page.click(continueButtonSelector);
 
-  await page.type('input[aria-label="Insira seu número de telefone."]', number);
+        await page.waitForSelector('.x1n2onr6.x78zum5.x1okw0bk.x6s0dn4.xl56j7k.x14atkfc.x1vd4hg5.xrxr3ny', { timeout: 60000 }); //codigo
 
-  // Aguarda 1 segundo
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  console.log('Aguardando botão "Avançar"...');
-  await page.waitForSelector('#app > div > div.landing-wrapper > div.landing-window > div.landing-main > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.xdt5ytf.x1qjc9v5 > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.xeuugli.x2lwn1j.xozqiw3.xamitd3.x12fk4p8.x1hq5gj4 > button');
-  await page.click('#app > div > div.landing-wrapper > div.landing-window > div.landing-main > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.xdt5ytf.x1qjc9v5 > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.xeuugli.x2lwn1j.xozqiw3.xamitd3.x12fk4p8.x1hq5gj4 > button');
+        const code = await page.evaluate(() => { //guarda o codigo em uma variavel
+          const spans = Array.from(document.querySelectorAll('.x1n2onr6.x78zum5.x1okw0bk.x6s0dn4.xl56j7k.x14atkfc.x1vd4hg5.xrxr3ny .x1c4vz4f span'));
+          return spans.map(span => span.innerText).join('');
+        });
 
-  console.log('Aguardando o código...');
-  await page.waitForSelector('.x1n2onr6.x78zum5.x1okw0bk.x6s0dn4.xl56j7k.x14atkfc.x1vd4hg5.xrxr3ny', { timeout: 60000 }); //aguarda nova tela
+        await browser.close();
+        res.json({ code });
+        return;
 
-  // Extrai o código dos spans
-  const codigo = await page.evaluate(() => {
-    const spans = Array.from(document.querySelectorAll('.x1n2onr6.x78zum5.x1okw0bk.x6s0dn4.xl56j7k.x14atkfc.x1vd4hg5.xrxr3ny .x1c4vz4f span'));
-    return spans.map(span => span.innerText).join(''); // Junte os textos
-  });
+      } catch (error) {
+        console.error("Erro ao encontrar botão, recarregando a página...", error);
 
-  console.log('Código recebido:', codigo || 'Nenhum código encontrado');
+        // Limpa o cache
+        await page.evaluate(() => {
+          caches.keys().then(function(names) {
+            for (let name of names) {
+              caches.delete(name);
+            }
+          });
+        });
+      
+        await page.reload({ waitUntil: 'networkidle0' });
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      }
+    }
+    
+  } catch (error) {
+    console.error("Erro ao capturar código", error);
+    res.status(500).json({ error: "Erro ao capturar o código" });
+  }
+});
 
-})();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor executado em http://localhost:${PORT}`);
+});
